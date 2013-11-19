2907ac18819f5c1c4002af261a7669402ce1bcf1
Quy Cao <quycao@vinasource.com>
2013-11-19 14:15:14 +0700

    Imported source code

---
+++ DemoApp/DemoApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DemoApp
+//
+//  Created by Cao Quoc Quy on 11/19/13.
+//  Copyright (c) 2013 Cao Quoc Quy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DemoApp/DemoApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DemoApp
+//
+//  Created by Cao Quoc Quy on 11/19/13.
+//  Copyright (c) 2013 Cao Quoc Quy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
